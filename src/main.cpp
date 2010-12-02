c74d2956487cd281cce289d1d0fd01a27170180f
A.Bekkine <abekkine@gmail.com>
2010-12-02 17:51:41 +0200

    - AI#0004 closed, by displaying version string on execution.  - Unused unistd header include removed.

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,12 @@
 #include <stdio.h>
-#include <unistd.h>
+#include <version.h>
 #include <GraWaves.h>
 
 int main( int argc, char *argv[] )
 {
+	// Display version information.
+	printf( "Hg.%s\n", VERSION_STRING );
+
 	GraWaves* gWaves = new GraWaves( atoi( argv[1] ) );
 
 	gWaves->Initialize();
