cfaf9515b4924afe28441cdcf9161c032f8f8af3
A.Bekkine <abekkine@gmail.com>
2010-12-06 16:46:02 +0200

    - Overloaded arithmetic operators implemented in Vector class.

--- inc/Vector.h
+++ inc/Vector.h
@@ -10,6 +10,24 @@ public:
 	Vector( double x, double y, double z );
 	// DONE : Implement either [Set] method or overload '=' operator.
 	void Set( double x, double y, double z );
+	// 1. Assignment
+	Vector& operator=(const Vector &rhs);
+	// 2. Addition
+	Vector operator+(const Vector &rhs);
+	// 3. Substraction
+	Vector operator-(const Vector &rhs);
+	// 4. Multiplication by a scalar
+	Vector operator*(const Scalar &rhs);
+	// 5. Division by a scalar
+	Vector operator/(const Scalar &rhs);
+	// 6. Dot product
+	Scalar operator|(const Vector &rhs);
+	// 7. Cross product
+	Vector operator^(const Vector &rhs);
+	// 8. Magnitude
+	Scalar operator~();
+	// 9. Unit
+	Vector operator!();
 	~Vector();
 
 	double x;
--- src/Vector.cpp
+++ src/Vector.cpp
@@ -1,3 +1,4 @@
+#include <math.h>
 #include <Vector.h>
 
 Vector::Vector()
@@ -24,4 +25,120 @@ void Vector::Set( double x, double y, double z )
 	Vector::y = y;
 	Vector::z = z;
 }
+
+// 1. Assignment
+Vector& Vector::operator=(const Vector &rhs)
+{
+	// DONE : Assignment overload.
+	if( this != &rhs )
+	{
+		this->x = rhs.x;
+		this->y = rhs.y;
+		this->z = rhs.z;
+	}
+
+	return *this;
+}
+
+// 2. Addition
+Vector Vector::operator+(const Vector &rhs)
+{
+	// DONE : Addition overload.
+	Vector result = *this;
+
+	result.x += rhs.x;
+	result.y += rhs.y;
+	result.z += rhs.z;
+
+	return result;
+}
+
+// 3. Substraction
+Vector Vector::operator-(const Vector &rhs)
+{
+	// DONE : Substraction overload.
+	Vector result = *this;
+
+	result.x -= rhs.x;
+	result.y -= rhs.y;
+	result.z -= rhs.z;
+
+	return result;
+}
+
+// 4. Multiplication by a scalar
+Vector Vector::operator*(const Scalar &rhs)
+{
+	Vector result = *this;
+
+	// DONE : Multiply by scalar overload.
+	result.x *= rhs;
+	result.y *= rhs;
+	result.z *= rhs;
+
+	return result;
+}
+
+// 5. Division by a scalar
+Vector Vector::operator/(const Scalar &rhs)
+{
+	Vector result = *this;
+
+	// DONE : Divide by scalar overload.
+	result.x /= rhs;
+	result.y /= rhs;
+	result.z /= rhs;
+
+	return result;
+}
+
+// 6. Dot product
+Scalar Vector::operator|(const Vector &rhs)
+{
+	// DONE : Dot product overload.
+	Scalar result;
+
+	result = (this->x * rhs.x);
+	result += (this->y * rhs.y);
+	result += (this->z * rhs.z);
+
+	return result;
+}
+
+// 7. Cross product
+Vector Vector::operator^(const Vector &rhs)
+{
+	// DONE : Cross product overload.
+	Vector result;
+
+	result.x = this->y * rhs.z - this->z * rhs.y;
+	result.y = this->z * rhs.x - this->x * rhs.z;
+	result.z = this->x * rhs.y - this->y * rhs.x;
+
+	return result;
+}
+
+// 8. Magnitude
+Scalar Vector::operator~()
+{
+	// DONE : Magnitude overload.
+	return sqrt( this->x*this->x + this->y*this->y + this->z*this->z );
+}
+
+// 9. Unit
+Vector Vector::operator!()
+{
+	Vector result = *this;
+	Scalar magnitude = ~result;
+
+	// DONE : Unit vector overload.
+	result.x /= magnitude;
+	result.y /= magnitude;
+	result.z /= magnitude;
+
+	return result;
+}
+
+
+
 
