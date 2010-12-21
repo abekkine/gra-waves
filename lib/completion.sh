97012cf75973a8764e3f33fd2a08753bdbd99c6d
A.Bekkine <abekkine@gmail.com>
2010-12-21 18:33:35 +0200

    - Completion statistics calculation updated (AI #0032).  - A new feature request (AI #0036) added.

--- lib/completion.sh
+++ lib/completion.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
-NUMTODO=$(grep -r "\/\/ TODO :" * | wc -l)
-NUMDONE=$(grep -r "\/\/ DONE :" * | wc -l)
-NUMTOTAL=$((NUMTODO+NUMDONE))
-PERCENT=$((100*NUMDONE/NUMTOTAL))
+CODE_TODO=$(grep -r "\/\/ TODO :" * | wc -l)
+CODE_DONE=$(grep -r "\/\/ DONE :" * | wc -l)
+CODE_TOTAL=$((CODE_TODO+CODE_DONE))
+CODE_PERCENT=$((100*CODE_DONE/CODE_TOTAL))
 
-echo "Completed/Total : $NUMDONE/$NUMTOTAL"
-echo "Completion Percetage : %$PERCENT"
+TODO_CLOSED=$(grep "| OPEN |" doc/todo.txt | wc -l)
+TODO_OPEN=$(grep "| CLOSED |" doc/todo.txt | wc -l)
+TODO_TOTAL=$((TODO_OPEN+TODO_CLOSED))
+TODO_PERCENT=$((100*TODO_CLOSED/TODO_TOTAL))
+
+echo "CODE : Completed/Total : $CODE_DONE/$CODE_TOTAL"
+echo "CODE : Completion Percentage : %$CODE_PERCENT"
+
+echo "TODO : Completed/Total : $TODO_CLOSED/$TODO_TOTAL"
+echo "TODO : Completion Percentage : %$TODO_PERCENT"
 
